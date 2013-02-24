13f1cf50fba61f46532a9e8669a53b64bd072415
David Saxon <davidesaxon@gmail.com>
2013-02-24 13:11:46 +1300

    first commit

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+/**************************************************************************\
+| This a simple script that takes a folder name that contains a sequence   |
+| of images and renames the image names so that any preceding zeros before |
+| numbers are removed.                                                     |
+|                                                                          |
+| @author David Saxon                                                      |
+\**************************************************************************/
+#include <iostream>
+
+//MAIN FUNCTION
+int main(int argc, char* argv[]) {
+
+
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
